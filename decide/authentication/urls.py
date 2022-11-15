d5a51c33662af29ca9e1edfb6217310af9eda525
borpincal <borpincal@alum.us.es>
2022-11-15 10:24:19 +0100

    feat(update): Implemented login
    Now you can login with a static view and url.

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -5,7 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import GetUserView, LogoutView, RegisterView, LoginView
 
 
-urlpatterns = [     
+urlpatterns = [ 
+    path('login-view/', LoginView.as_view()),     
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
